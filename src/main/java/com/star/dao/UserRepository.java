f59b3d508755ef09a4622ba4a319781a08e00da4
chiwenqiang <13858175347@sina.cn>
2020-07-06 18:00:56 +0800

    2020/7/6 cwq：         1.前台页面加入 后台管理按钮，直达后台管理登陆界面，         2.删除 友人说 功能，建议加在后台登陆页面，或者注册页面         3. 局部修改：                     1）更改前后台页面网站创建时间         4. 增加后台注册功能：用户名唯一

--- src/main/java/com/star/dao/UserRepository.java
+++ src/main/java/com/star/dao/UserRepository.java
@@ -11,4 +11,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByUsernameAndPassword(String username, String password);
+
+
+
+       User findByUsername(String username);
+//
+//        void persist(String username, String password);
 }
